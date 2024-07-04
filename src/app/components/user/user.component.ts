deb48b8a3df95ed0a4cbc0383d05403c24c875aa
lesykien <lesykien030@gmail.com>
2024-07-04 14:17:00 +0700

    create nav of responsevi

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
-export class UserComponent {
-
+export class UserComponent implements OnInit {
+  isNav: boolean = false;
+  ngOnInit(): void {}
 }
