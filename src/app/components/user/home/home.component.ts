e93542189c499c1b7997a5d951cd13a7998f057d
lesykien <lesykien030@gmail.com>
2024-07-05 10:38:58 +0700

    create display home page not responsevi

--- src/app/components/user/home/home.component.ts
+++ src/app/components/user/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
+  list: any[] = [1, 2, 3, 4];
+  items: any[] = [1, 2, 3, 4, 1, 1, 1, 1];
+  blog: any[] = [1, 1, 1];
   ngOnInit(): void {}
-
 }
