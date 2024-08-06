d933725149e9d6545a9b01afd4fbdec8282130d3
TUAN <hoangquoctuans3@gmail.com>
2024-08-07 04:22:39 +0700

    lấy dữ liêu wishList ra view

--- src/app/components/user/favorite/favorite.component.ts
+++ src/app/components/user/favorite/favorite.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { WishListService } from './../../../services/wishlist.service';
+import { Component, Input } from '@angular/core';
+import { productsUsingShop } from '../../../model/product.model';
 
 @Component({
   selector: 'app-favorite',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './favorite.component.scss'
 })
 export class FavoriteComponent {
+  products: productsUsingShop[] = [];
+  selectedColorIndices: number[] = [];
+  selectedSizeIndices: number[] = [];
 
+  constructor(private wishListService: WishListService) { }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  async loadData() {
+    console.log('loadData được gọi');
+    this.wishListService.getData().subscribe((res) => {
+      console.log('Dữ liệu được nhận:', res);
+      this.products = res.data;
+      console.log('Sản phẩm được gán:', this.products);
+      this.processData();
+    }, (error) => {
+      console.log('Lỗi khi nhận dữ liệu:', error);
+    });
+  }
+
+  processData() {
+    console.log('processData được gọi');
+    this.selectedColorIndices = Array(this.products.length).fill(0);
+    this.selectedSizeIndices = Array(this.products.length).fill(0);
+    console.log('Mảng màu và kích thước được tạo:', this.selectedColorIndices, this.selectedSizeIndices);
+  }
 }
---
+++ src/app/services/wishlist.service.ts
@@ -0,0 +1,24 @@
+import { AuthService } from './auth.service';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environment/environment.bassic';
+import { Observable } from 'rxjs';
+import { singleResponse } from '../model/response.model';
+import { productsDtos, productsUsingShop } from '../model/product.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WishListService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
+  
+  getToken() {
+    return this.authService.getTokenUser();
+  }
+
+  getData(): Observable<singleResponse<productsUsingShop[]>> {
+    return this.http.get<singleResponse<productsUsingShop[]>>(
+      `${environment.api}/User/get-wishlist-product-${this.getToken()}`,
+    );
+  }
+}
