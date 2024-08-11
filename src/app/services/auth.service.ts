401b52d39b452e13f5974ebf5de5d46cd51a1078
kiên lê <lesykien030@gmail.com>
2024-08-11 18:21:11 +0700

    fix lỗi

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,18 +5,14 @@ import { catchError, map, Observable } from 'rxjs';
 import { singleResponse } from '../model/response.model';
 import { environment } from '../environment/environment.bassic';
 import { IAuth } from '../interface/auth.interface';
-import { SignUpComponent } from '../components/sign-up/sign-up.component';
-import { error } from 'console';
 import { CookieService } from 'ngx-cookie-service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  getCookie(token: string) {
-    throw new Error('Method not implemented.');
-  }
-  constructor(private http: HttpClient, @Inject('IAuth') private auth: IAuth, private cookieService: CookieService ) { }
+  constructor(private http: HttpClient, @Inject('IAuth') private auth: IAuth, private cookieService: CookieService, private toastrServices: ToastrService) { }
   private tokenKey = 'TokenUser';
 
   signIn(request: ISignInRequest): Observable<singleResponse<string>> {
@@ -27,6 +23,10 @@ export class AuthService {
       )
       .pipe(
         map((response) => {
+          if (!response) {
+            this.toastrServices.error("Kiểm trả lại tên đăng nhập và mật khẩu", "Thông báo")
+            return response;
+          }
           this.auth.setCookie(
             'TokenUser',
             response.data,
@@ -38,6 +38,7 @@ export class AuthService {
           return response;
         }),
         catchError((err) => {
+          this.toastrServices.error("Kiểm trả lại tên đăng nhập và mật khẩu", "Thông báo")
           throw err;
         })
       );
