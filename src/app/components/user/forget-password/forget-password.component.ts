15ce34b62ecffc4b554b89f248d0a59facae68ca
TUAN <hoangquoctuans3@gmail.com>
2024-08-14 21:48:30 +0700

    fix change password

--- src/app/components/user/forget-password/forget-password.component.ts
+++ src/app/components/user/forget-password/forget-password.component.ts
@@ -3,52 +3,54 @@ import { UserService } from '../../../services/user.service';
 import { Component } from '@angular/core';
 import { passwordMatchValidator } from '../../sign-up/sign-up.component';
 import { log } from 'console';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-forget-password',
   templateUrl: './forget-password.component.html',
-  styleUrl: './forget-password.component.scss'
+  styleUrl: './forget-password.component.scss',
 })
 export class ForgetPasswordComponent {
-  constructor(
-    private userService: UserService,
-    private form: FormBuilder
-  ) { }
+  constructor(private userService: UserService, private form: FormBuilder,
+    private toastrServices: ToastrService,
+  ) {}
 
   submitted = false;
 
-  registerform = this.form.group({
-    token : [''],
-    currentPassword: ['', [Validators.required]],
-    password: ['', [Validators.required, Validators.maxLength(15)]],
-    confirmPassword: ['', [Validators.required]]   
-  },{ validators: passwordMatchValidator });
-
+  registerform = this.form.group(
+    {
+      token: [''],
+      currentPassword: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.maxLength(15)]],
+      confirmPassword: ['', [Validators.required]],
+    },
+    { validators: passwordMatchValidator }
+  );
 
   async ChangePassWord() {
     try {
       console.log(this.registerform.value);
       this.submitted = true;
-  if (this.registerform.invalid) {
-    console.log("invalid");
-    console.log(this.registerform.errors?.['passwordMismatch'] + "????");
-    this.registerform.markAllAsTouched();
-    Object.keys(this.registerform.controls).forEach(key => {
-      const control = this.registerform.get(key);
-      if (control?.invalid) {
-        console.log(`Lỗi tại trường ${key}:`);
-        Object.keys(control.errors || {}).forEach(errorKey => {
-          console.log(`${errorKey}: ${control.errors?.[errorKey]}`);
+      if (this.registerform.invalid) {
+        console.log('invalid');
+        console.log(this.registerform.errors?.['passwordMismatch'] + '????');
+        this.registerform.markAllAsTouched();
+        Object.keys(this.registerform.controls).forEach((key) => {
+          const control = this.registerform.get(key);
+          if (control?.invalid) {
+            console.log(`Lỗi tại trường ${key}:`);
+            Object.keys(control.errors || {}).forEach((errorKey) => {
+              console.log(`${errorKey}: ${control.errors?.[errorKey]}`);
+            });
+          }
         });
+        return;
       }
-    });
-    return;
-  }
       this.userService.ChangePassWord(this.registerform.value).subscribe(
         async (res) => {
           if (res) {
             console.log(res.messsage);
-            await alert(res.messsage);
+            await this.toastrServices.success(res.messsage);
           }
         },
         async (error) => {
